e1d939ad729338a327b6d40b64cdc910392e172d
RyanFlachman <ryan@flachman.net>
2019-05-25 10:51:50 -0600

    Add change round functionality

--- src/FastMoney.js
+++ src/FastMoney.js
@@ -3,25 +3,25 @@ import Turn from './Turn';
 import Game from './Game';
 
 class FastMoney extends Round {
-	constructor(question, answers, currentGame, turnNumber, multiplier) {
-		super(question, answers, currentGame, turnNumber)
-		this.player1Guesses = [];
-		this.player2Guesses = [];
-		this.multiplier = multiplier || 1;
-	}
+  constructor(question, answers, currentGame, turnNumber, multiplier) {
+    super(question, answers, currentGame, turnNumber)
+    this.player1Guesses = [];
+    this.player2Guesses = [];
+    this.multiplier = multiplier || 1;
+  }
 
-	evaluateGuesses(guess) {
-		this.answers.filter(answer => {
-			if(answer === guess) {
-				this.player1Guesses.push(guess)
-			} 
-		});
-	}
+  evaluateGuesses(guess) {
+    this.answers.filter(answer => {
+      if(answer === guess) {
+        this.player1Guesses.push(guess)
+      } 
+    });
+  }
 
-	multiplyScore(guesses) {
-		console.log(this.currentGame)
-		return this.player1Guesses * this.multiplier;
-	}
+  multiplyScore(guesses) {
+    console.log(this.currentGame)
+    return this.player1Guesses * this.multiplier;
+  }
 }
 
 export default FastMoney;
